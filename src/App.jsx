8beb3c1354a022af11cf20c663fe69df7db3aeaf
John Nkp <john.nkp1@gmail.com>
2024-02-04 22:19:10 +1100

    build: initial setup of vite app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import PostsList from './features/posts/PostsList'
+
+function App() {
+  return (
+    <div>
+      <h1>React on rails blog</h1>
+      <p>Find this application in layout in client/src/App.jsx</p>
+      <PostsList />
+    </div>
+  )
+}
+
+export default App
